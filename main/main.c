e001e3045e5dc23a12a72a1bf8fae2c64ca5236d
Nick Donaldson <ndonald2@gmail.com>
2022-03-03 15:26:58 -0700

    Working CV/Gate

--- main/main.c
+++ main/main.c
@@ -2,6 +2,7 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "driver/i2c.h"
+#include "driver/gpio.h"
 #include "esp_console.h"
 #include "esp_log.h"
 #include "linenoise/linenoise.h"
@@ -10,15 +11,43 @@
 #define TAG "ble-midi-cv"
 #define PROMPT_STR CONFIG_IDF_TARGET
 
+#define GATE_PIN 27
+
 #define DAC_I2C_SDA 21
 #define DAC_I2C_SCL 22
 #define DAC_I2C_FREQ 400000
 #define DAC_I2C_ADDR 0x60
+#define DAC_VMAX 3.3f
 
+#define NN_V0 24 // C1
 
-void callback_midi_message_received(uint8_t blemidi_port, uint16_t timestamp, uint8_t midi_status, uint8_t *remaining_message, size_t len, size_t continued_sysex_pos)
+static uint8_t last_note;
+static bool note_on = false;
+
+uint16_t nn_to_dac_value(uint8_t nn)
 {
-    ESP_LOGI(TAG, "CALLBACK blemidi_port=%d, timestamp=%d, midi_status=0x%02x, len=%d, continued_sysex_pos=%d, remaining_message:", blemidi_port, timestamp, midi_status, len, continued_sysex_pos);
+    uint8_t nn_scaled = (nn < NN_V0 ? NN_V0 : nn) - NN_V0;
+    float oct_mult = (float)(nn_scaled / (12.0f * DAC_VMAX));
+    int value = (int)(oct_mult * 4095.0f);
+    ESP_LOGI(TAG, "Raw nn:%d, Scaled nn:%d, DAC value: %d", nn, nn_scaled, value);
+    return value > 4095 ? 4095 : value;
+}
+
+void gpio_gate_init(void)
+{
+    gpio_config_t io_conf;
+    //disable interrupt
+    io_conf.intr_type = GPIO_PIN_INTR_DISABLE;
+    //set as output mode
+    io_conf.mode = GPIO_MODE_OUTPUT;
+    //bit mask of the pins that you want to set,e.g.GPIO18/19
+    io_conf.pin_bit_mask = (1ULL << GATE_PIN);
+    //disable pull-down mode
+    io_conf.pull_down_en = 0;
+    //disable pull-up mode
+    io_conf.pull_up_en = 0;
+    //configure GPIO with the given settings
+    gpio_config(&io_conf);
 }
 
 esp_err_t i2c_dac_init(void)
@@ -68,8 +97,45 @@ void start_console(void)
     ESP_ERROR_CHECK(esp_console_start_repl(repl));
 }
 
+void callback_midi_message_received(uint8_t blemidi_port, uint16_t timestamp, uint8_t midi_status, uint8_t *remaining_message, size_t len, size_t continued_sysex_pos)
+{
+    ESP_LOGI(TAG, "CALLBACK blemidi_port=%d, timestamp=%d, midi_status=0x%02x, len=%d, continued_sysex_pos=%d, remaining_message:", blemidi_port, timestamp, midi_status, len, continued_sysex_pos);
+    uint8_t code = midi_status >> 4;
+    // uint8_t channel = midi_status & 0x0F;
+    if (code == 0x09) // note on, ignore channel
+    {
+        last_note = remaining_message[0];
+        if (note_on)
+        {
+            // gate off, update CV, gate on
+            gpio_set_level(GATE_PIN, 0);
+            i2c_dac_write(nn_to_dac_value(last_note));
+            vTaskDelay(pdMS_TO_TICKS(1));
+            gpio_set_level(GATE_PIN, 1);
+        }
+        else
+        {
+            // update CV, gate on
+            i2c_dac_write(nn_to_dac_value(last_note));
+            gpio_set_level(GATE_PIN, 1);
+        }
+        note_on = true;
+    }
+    else if (code == 0x08) // note off
+    {
+        // Only stop gate if same note off
+        if (last_note == remaining_message[0])
+        {
+            gpio_set_level(GATE_PIN, 0);
+            note_on = false;
+        }
+    }
+}
+
 void app_main(void)
 {
+//    esp_log_level_set(TAG, ESP_LOG_INFO);
+//    setup_console();
 
     int status = blemidi_init(callback_midi_message_received);
     if (status < 0)
@@ -81,14 +147,6 @@ void app_main(void)
         ESP_LOGI(TAG, "BLE MIDI Driver initialized successfully");
     }
 
-//    setup_console();
+    gpio_gate_init();
     ESP_ERROR_CHECK(i2c_dac_init());
-
-    uint16_t val = 0;
-    while (1)
-    {
-        ESP_ERROR_CHECK(i2c_dac_write(val)); 
-        val = (val + 1) % 4096;
-        vTaskDelay(pdMS_TO_TICKS(10));
-    }
 }
